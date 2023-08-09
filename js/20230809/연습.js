9867cc2b234c181fad9d9fb64495070178d565e4
nonelijah <elia341947@gmail.com>
2023-08-09 15:55:37 +0900

    20230809 수업내용

---
+++ js/20230809/연습.js
@@ -0,0 +1,33 @@
+let menu;
+
+function order() {
+    switch (menu) {
+        case "아메리카노":
+            price = 4000;
+            break;
+        case "카페라떼":
+            price = 5000;
+            break;
+        case "바닐라라떼":
+            price = 6000;
+            break;
+        case "콜드브루":
+            price = 5500;
+            break;
+        case "딸기라떼":
+            price = 7000;
+            break;
+        case "레몬에이드":
+            price = 6500;
+            break;
+        case "에스프레소":
+            price = 3500;
+            break;
+        case "루이보스":
+            price = 4500;
+            break;
+        default:
+            console.log("메뉴를 정확히 입력하세요.");
+            break;
+    }
+}
