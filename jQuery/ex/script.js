6eac0d04e0ccdf72a3c1430521c4ce6c07949dd9
nonelijah <elia341947@gmail.com>
2023-08-29 17:44:02 +0900

    add: 이미지갤러리 - 키보드핸들링 기능 추가

---
+++ jQuery/ex/script.js
@@ -0,0 +1,40 @@
+const viewFrame = document.querySelector(".view-frame");
+const ul = document.querySelector(".img-container");
+const lis = document.querySelectorAll("li");
+
+// changeImg
+function changeImg() {
+    const targetLi = document.querySelector("li.selected");
+    viewFrame.innerHTML = "";
+    viewFrame.innerHTML = targetLi.innerHTML;
+}
+
+// keyboardHandle
+function keyboardHandle(e) {
+    const firstLi = lis[0],
+        lastLi = lis[lis.length - 1],
+        selectLi = document.querySelector("li.selected"),
+        preLi = selectLi.previousElementSibling,
+        nextLi = selectLi.nextElementSibling;
+
+    // console.log(e.key);
+    if (e.key === "ArrowUp" || e.key === "ArrowLeft") {
+        selectLi.classList.remove("selected");
+        if (selectLi === firstLi) {
+            lastLi.classList.add("selected");
+        } else {
+            preLi.classList.add("selected");
+        }
+    }
+    if (e.key === "ArrowDown" || e.key === "ArrowRight") {
+        selectLi.classList.remove("selected");
+        if (selectLi === lastLi) {
+            firstLi.classList.add("selected");
+        } else {
+            nextLi.classList.add("selected");
+        }
+    }
+    changeImg();
+}
+
+document.addEventListener("keydown", keyboardHandle);
