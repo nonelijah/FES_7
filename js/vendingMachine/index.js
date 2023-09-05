bc295cec867c339ee8c8bcd20caa32438604e50b
nonelijah <elia341947@gmail.com>
2023-09-05 16:25:45 +0900

    add: 20230905 수업정리

--- js/vendingMachine/index.js
+++ js/vendingMachine/index.js
@@ -1,4 +1,8 @@
 import ColaGenerator from "./js/colaGenerator.js";
+import VendingMachineFunc from "./js/vendingMachineFunc.js";
 
 const colaGenerator = new ColaGenerator();
-colaGenerator.setup();
+await colaGenerator.setup();
+
+const vendingMachineFunc = new VendingMachineFunc();
+vendingMachineFunc.setup();
