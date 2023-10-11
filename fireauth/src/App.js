23e6b5b005a8b5ef8d8ff8782d86ff9a5358e027
nonelijah <elia341947@gmail.com>
2023-10-11 16:02:56 +0900

    두근두근 다이어리
    pages: Home, Login, Signup
    components : Header
    connect: firebase

--- fireauth/src/App.js
+++ fireauth/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/signup" element={<Signup />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ fireauth/src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+function home() {
+    return <div>Home</div>;
+}
+
+export default home;
